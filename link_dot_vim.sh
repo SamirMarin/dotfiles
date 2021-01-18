81066a98f91731eb224da96e487a6de3cbf2ae5f
SamirMarin <marin.samir@gmail.com>
2021-01-17 20:37:55 -0800

    Add logic to link_dot_vim.sh to install plug Change color scheme Add gitignore

--- link_dot_vim.sh
+++ link_dot_vim.sh
@@ -6,3 +6,15 @@ NEOVIM_VIMRC_DIR=".config/"
 ln -nfs "$PWD/$VIM_VIMRC_DIR" "${HOME}/"
 ln -nfs "$PWD/$NEOVIM_VIMRC_DIR" "${HOME}/"
 
+PLUG_VIM_FILE="${HOME}/.vim/autoload/plug.vim"
+PLUG_NVIM_FILE="${HOME}/.local/share/nvim/site/autoload/plug.vim"
+
+if [ ! -f ${PLUG_VIM_FILE} ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+if [ ! -f ${PLUG_NVIM_FILE} ]; then
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
+
